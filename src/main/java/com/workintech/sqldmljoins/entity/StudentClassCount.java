7247779a6d0e5390b8eb60c061ff1da99f12cbe7
Umut Tekin <tekinumut9@gmail.com>
2026-03-30 06:47:10 +0300

    Sprint 18 Gün 1 Projesi Tamamlandı.

--- src/main/java/com/workintech/sqldmljoins/entity/StudentClassCount.java
+++ src/main/java/com/workintech/sqldmljoins/entity/StudentClassCount.java
@@ -5,11 +5,15 @@ import jakarta.persistence.ConstructorResult;
 import jakarta.persistence.NamedNativeQuery;
 import jakarta.persistence.SqlResultSetMapping;
 
-@NamedNativeQuery(name="findStudentClassCount", query = "SELECT sinif, COUNT(sinif) FROM ogrenci GROUP BY sinif",
+@NamedNativeQuery(name = "findStudentClassCount",
+        query = "SELECT sinif, COUNT(*) as count FROM ogrenci GROUP BY sinif",
         resultSetMapping = "Mapping.StudentClassCount")
-@SqlResultSetMapping(name="Mapping.StudentClassCount", classes = @ConstructorResult(targetClass = StudentClassCount.class,
-        columns = {@ColumnResult(name = "sinif"),
-                @ColumnResult(name = "count")}))
+@SqlResultSetMapping(name = "Mapping.StudentClassCount",
+        classes = @ConstructorResult(targetClass = StudentClassCount.class,
+                columns = {
+                        @ColumnResult(name = "sinif"),
+                        @ColumnResult(name = "count")
+                }))
 public interface StudentClassCount {
     String getSinif();
     int getCount();
